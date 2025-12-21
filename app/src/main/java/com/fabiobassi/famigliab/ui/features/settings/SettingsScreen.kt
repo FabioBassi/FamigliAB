e945237add1020ccee48fdc80c0a61032b991778
fabiobassi <bassi.fabio11@gmail.com>
2025-12-21 14:39:11 +0100

    Fixed fatal loading data bugs

--- app/src/main/java/com/fabiobassi/famigliab/ui/features/settings/SettingsScreen.kt
+++ app/src/main/java/com/fabiobassi/famigliab/ui/features/settings/SettingsScreen.kt
@@ -1,12 +1,20 @@
 package com.fabiobassi.famigliab.ui.features.settings
 
+import android.content.Context
 import android.widget.Toast
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -16,12 +24,13 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.fabiobassi.famigliab.R
 import com.fabiobassi.famigliab.ui.theme.FamigliABTheme
 import java.io.File
 import java.io.FileOutputStream
@@ -29,13 +38,36 @@ import java.io.FileOutputStream
 @Composable
 fun SettingsScreen(paddingValues: PaddingValues) {
     val context = LocalContext.current
-    var showPaswordResetDialog by remember { mutableStateOf(false) }
+    val sharedPreferences = remember {
+        context.getSharedPreferences("color_prefs", Context.MODE_PRIVATE)
+    }
+    var showPasswordResetDialog by remember { mutableStateOf(false) }
     var showImportPaymentCsvDialog by remember { mutableStateOf(false) }
     var showDeleteAllDataDialog by remember { mutableStateOf(false) }
     var showDeletePoopTrackerCsvDialog by remember { mutableStateOf(false) }
+    var showColorSettingDialog by remember { mutableStateOf(false) }
+    var personToSetColorFor by remember { mutableStateOf<String?>(null) }
+
+    val (fabColorHex, setFabColorHex) = remember { mutableStateOf(sharedPreferences.getString("fab_color", null)) }
+    val (sabColorHex, setSabColorHex) = remember { mutableStateOf(sharedPreferences.getString("sab_color", null)) }
+
+    val fabColor = remember(fabColorHex) {
+        try {
+            fabColorHex?.let { Color(android.graphics.Color.parseColor(it)) } ?: Color.Transparent
+        } catch (e: Exception) {
+            Color.Transparent
+        }
+    }
+    val sabColor = remember(sabColorHex) {
+        try {
+            sabColorHex?.let { Color(android.graphics.Color.parseColor(it)) } ?: Color.Transparent
+        } catch (e: Exception) {
+            Color.Transparent
+        }
+    }
 
     val importPasswordsLauncher = rememberLauncherForActivityResult(
-        contract = ActivityResultContracts.GetContent()
+        contract = ActivityResultContracts.GetContent(),
     ) { uri ->
         uri?.let {
             try {
@@ -57,81 +89,121 @@ fun SettingsScreen(paddingValues: PaddingValues) {
         modifier = Modifier
             .fillMaxSize()
             .padding(paddingValues) // Apply outer padding here
-            .padding(16.dp) // Apply inner padding for content spacing
+            .padding(16.dp), // Apply inner padding for content spacing
     ) {
         Text(
-            text = "Home",
+            text = "General",
             style = MaterialTheme.typography.headlineMedium,
             fontWeight = FontWeight.Bold,
-            modifier = Modifier.padding(bottom = 16.dp)
+            modifier = Modifier.padding(bottom = 16.dp),
         )
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.padding(bottom = 16.dp),
+        ) {
+            TextButton(
+                onClick = {
+                    personToSetColorFor = "Fab"
+                    showColorSettingDialog = true
+                },
+            ) {
+                Text("Set Fab color")
+            }
+            Spacer(modifier = Modifier.width(8.dp))
+            Box(
+                modifier = Modifier
+                    .size(24.dp)
+                    .background(fabColor)
+                    .border(1.dp, Color.Black),
+            )
+        }
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.padding(bottom = 16.dp),
+        ) {
+            TextButton(
+                onClick = {
+                    personToSetColorFor = "Sab"
+                    showColorSettingDialog = true
+                },
+            ) {
+                Text("Set Sab color")
+            }
+            Spacer(modifier = Modifier.width(8.dp))
+            Box(
+                modifier = Modifier
+                    .size(24.dp)
+                    .background(sabColor)
+                    .border(1.dp, Color.Black),
+            )
+        }
         Text(
             text = "Budgeting",
             style = MaterialTheme.typography.headlineMedium,
             fontWeight = FontWeight.Bold,
-            modifier = Modifier.padding(bottom = 16.dp)
+            modifier = Modifier.padding(bottom = 16.dp),
         )
         TextButton(
             onClick = { showImportPaymentCsvDialog = true },
-            modifier = Modifier.padding(bottom = 16.dp)
+            modifier = Modifier.padding(bottom = 16.dp),
         ) {
             Text("Import payment csv")
         }
         TextButton(
             onClick = { showDeleteAllDataDialog = true },
-            modifier = Modifier.padding(bottom = 16.dp)
+            modifier = Modifier.padding(bottom = 16.dp),
         ) {
             Text(
                 text = "Delete all data",
-                color = MaterialTheme.colorScheme.error
+                color = MaterialTheme.colorScheme.error,
             )
         }
         Text(
             text = "Poop tracker",
             style = MaterialTheme.typography.headlineMedium,
             fontWeight = FontWeight.Bold,
-            modifier = Modifier.padding(bottom = 16.dp)
+            modifier = Modifier.padding(bottom = 16.dp),
         )
         TextButton(
             onClick = { showDeletePoopTrackerCsvDialog = true },
-            modifier = Modifier.padding(bottom = 16.dp)
+            modifier = Modifier.padding(bottom = 16.dp),
         ) {
             Text(
                 text = "Delete all data",
-                color = MaterialTheme.colorScheme.error
+                color = MaterialTheme.colorScheme.error,
             )
         }
         Text(
             text = "Passwords",
             style = MaterialTheme.typography.headlineMedium,
             fontWeight = FontWeight.Bold,
-            modifier = Modifier.padding(bottom = 8.dp) // Adjusted padding for the button below
+            modifier = Modifier.padding(bottom = 8.dp), // Adjusted padding for the button below
         )
         TextButton(
-            onClick = { showPaswordResetDialog = true },
-            modifier = Modifier.padding(bottom = 16.dp)
+            onClick = { showPasswordResetDialog = true },
+            modifier = Modifier.padding(bottom = 16.dp),
         ) {
             Text("Import Passwords.json")
         }
 
-        if (showPaswordResetDialog) {
+        if (showPasswordResetDialog) {
             AlertDialog(
-                onDismissRequest = { showPaswordResetDialog = false },
+                onDismissRequest = { showPasswordResetDialog = false },
                 title = { Text("Confirm Import") },
                 text = { Text("Are you sure you want to import passwords? This will overwrite existing data.") },
                 confirmButton = {
                     TextButton(onClick = {
-                        showPaswordResetDialog = false
+                        showPasswordResetDialog = false
                         importPasswordsLauncher.launch("application/json")
                     }) {
                         Text("Confirm")
                     }
                 },
                 dismissButton = {
-                    TextButton(onClick = { showPaswordResetDialog = false }) {
+                    TextButton(onClick = { showPasswordResetDialog = false }) {
                         Text("Cancel")
                     }
-                }
+                },
             )
         }
 
@@ -171,15 +243,28 @@ fun SettingsScreen(paddingValues: PaddingValues) {
                     TextButton(onClick = { showDeletePoopTrackerCsvDialog = false }) {
                         Text("Cancel")
                     }
-                }
+                },
             )
+        }
+
+        if (showColorSettingDialog) {
+            personToSetColorFor?.let {
+                ColorSettingDialog(
+                    person = it,
+                    onDismissRequest = {
+                        showColorSettingDialog = false
+                        setFabColorHex(sharedPreferences.getString("fab_color", null))
+                        setSabColorHex(sharedPreferences.getString("sab_color", null))
+                    },
+                )
+            }
         }
 
         Text(
             text = "Documents",
             style = MaterialTheme.typography.headlineMedium,
             fontWeight = FontWeight.Bold,
-            modifier = Modifier.padding(bottom = 16.dp)
+            modifier = Modifier.padding(bottom = 16.dp),
         )
     }
 }
